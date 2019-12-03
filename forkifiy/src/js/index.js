ed3c48a8f67d7581a5d0bd3a089f2175d9204c4b
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-12-03 21:49:45 +0100

    Working get

--- forkifiy/src/js/index.js
+++ forkifiy/src/js/index.js
@@ -1,6 +1,33 @@
-// Global app controller
-import num from './test';
+import Search from "./models/Search";
 
-const x = 23;
+/** Global state of the app
+* - Search object'
+* - Current recipe object
+* - Shopping list object
+* - Liked recipes
+* */
+const state = {};
 
-console.log(`I imported ${num} from another module named test.js! Variable x is ${x}`);
+const controlSearch = async () => {
+    //get query from view
+    const query = 'pizza'; //TODO
+
+    if(query) {
+        //new search object and add to state
+        state.search = new Search(query);
+
+        //prepare UI
+
+        //do search
+        await state.search.getResults();
+
+        //render results on UI
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector('.search')
+    .addEventListener('submit', e=>{
+        e.preventDefault();
+        controlSearch();
+    });
